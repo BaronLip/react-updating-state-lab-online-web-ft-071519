b6d0e8cf54ab62bf1611082420615da58707e57c
BaronLip <baronlip@gmail.com>
2019-11-17 20:36:10 -0600

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,35 @@
-// Code DigitalClicker Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            // hasBeenClicked: false,
+            timesClicked: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState(previousState => {
+            return {
+                // hasBeenClicked: true,
+                timesClicked: previousState.timesClicked + 1
+            }
+        })
+        // console.log(this.state.timesClicked)
+        // console.log(this.state.hasBeenClicked)
+        if (this.state.hasBeenClicked === true) {
+            const label = document.getElementsByTagName("label");
+            label[0].innerText = "Clicked"
+        }
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                
+                <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+            </React.Fragment>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,59 @@
-// Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    changeBitrate = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12,
+            }
+        })
+    }
+
+    // In order to dive deeper into an object, use multiple spread operators. 
+    changeRes = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    ...this.state.settings.video,
+                    resolution: "720p"
+                }
+            }
+        })            
+        console.log(this.state.settings)
+    }
+    
+    // // This version only merges the state of resolution
+    // changeRes = () => {
+    //     this.setState({
+    //         settings: Object.assign({}, this.state.settings.video, {
+    //             resolution: "720"
+    //         })            
+    //     })
+    //     console.log(this.state.settings)
+    // }
+
+    render() {
+        return (
+            <React.Fragment>
+                <button className="bitrate" onClick={this.changeBitrate}>Bitrate</button>
+                <button className="resolution" onClick={this.changeRes}>Res</button>
+            </React.Fragment>
+        )
+    }
+}
